206d2be6d0e4989996074a82b441693e52d4ac47
yzw0917 <59817782@qq.com>
2020-02-04 16:16:59 +0800

    20200204

--- random001.py
+++ random001.py
@@ -1,8 +1,9 @@
 import random
 
-r_ip=["1.1.1.1",
-      "2.2.2.2",
-      "3.3.3.3"]
+r_ip=["192.168.10.63",
+      "20.15.25.142",
+      "30.123.53.35",
+      "987.123.324.556"]
 for i in range(1,100):
       print(random.choice(r_ip))
-      print(random.randint(1,100))
+      print(random.randint(1,10000))
